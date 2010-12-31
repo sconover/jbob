13fcd29d8b781c6322c677b9b59608738db903b0
Steve Conover <sconover@gmail.com>
2010-12-31 11:02:01 -0800

    basic attributes

--- spec/node_spec.js
+++ spec/node_spec.js
@@ -32,48 +32,48 @@ describe("Jbob: nodes", function() {
     
   });
   
-  // 
-  // describe("attributes", function() {
-  //   it("renders attributes as key value pairs separated by = in the tag", function(){
-  //     expect(fooBar.setAttributes({a:"b"}).render()).
-  //    toEqual("<fooBar a=\"b\"/>\n");
-  //   });
-  // 
-  //   it("renders several attributes in a row.  order is imposed", function(){
-  //     expect(fooBar.setAttributes({x:"y", a:"b", c:"d"}).render()).
-  //    toEqual("<fooBar a=\"b\" c=\"d\" x=\"y\"/>\n");
-  //   });
-  // 
-  //   it("renders booleans", function(){
-  //     expect(fooBar.setAttributes({a:true}).render()).toEqual("<fooBar a=\"true\"/>\n");
-  //     expect(fooBar.setAttributes({a:false}).render()).toEqual("<fooBar a=\"false\"/>\n");
-  //   });
-  // 
-  //   it("renders integers", function(){
-  //     expect(fooBar.setAttributes({a:0}).render()).toEqual("<fooBar a=\"0\"/>\n");
-  //     expect(fooBar.setAttributes({a:-1}).render()).toEqual("<fooBar a=\"-1\"/>\n");
-  //     expect(fooBar.setAttributes({a:7}).render()).toEqual("<fooBar a=\"7\"/>\n");
-  //   });
-  //   
-  //   it("renders floats", function(){
-  //     expect(fooBar.setAttributes({a:0.0}).render()).toEqual("<fooBar a=\"0\"/>\n");
-  // 
-  //     expect(fooBar.setAttributes({a:0.01}).render()).toEqual("<fooBar a=\"0.01\"/>\n");
-  //     expect(fooBar.setAttributes({a:0.0100}).render()).toEqual("<fooBar a=\"0.01\"/>\n");
-  // 
-  //     expect(fooBar.setAttributes({a:23.45}).render()).toEqual("<fooBar a=\"23.45\"/>\n");
-  //     expect(fooBar.setAttributes({a:-23.45}).render()).toEqual("<fooBar a=\"-23.45\"/>\n");
-  //   });
-  //   
-  //   it("does the javascript thing on really big and really small numbers.  just calling this out.", function(){
-  //     expect(fooBar.setAttributes({a:70000000000000000000000}).render()).toEqual("<fooBar a=\"7e+22\"/>\n");
-  //     expect(fooBar.setAttributes({a:70000000000000000000000.01}).render()).toEqual("<fooBar a=\"7e+22\"/>\n");
-  //     
-  //     expect(fooBar.setAttributes({a:0.00000000000000000000007}).render()).toEqual("<fooBar a=\"7e-23\"/>\n");
-  //   });
-  //   
-  //   //todo: throw if the attribute value is a non-primitive?
-  // });
+  
+  describe("attributes", function() {
+    it("renders attributes as key value pairs separated by = in the tag", function(){
+      expect(_.fooBar({a:"b"})).
+     toEqual("<fooBar a=\"b\"/>\n");
+    });
+  
+    it("renders several attributes in a row.  order is imposed", function(){
+      expect(_.fooBar({x:"y", a:"b", c:"d"})).
+     toEqual("<fooBar a=\"b\" c=\"d\" x=\"y\"/>\n");
+    });
+  
+    // it("renders booleans", function(){
+    //   expect(fooBar.setAttributes({a:true}).render()).toEqual("<fooBar a=\"true\"/>\n");
+    //   expect(fooBar.setAttributes({a:false}).render()).toEqual("<fooBar a=\"false\"/>\n");
+    // });
+    //   
+    // it("renders integers", function(){
+    //   expect(fooBar.setAttributes({a:0}).render()).toEqual("<fooBar a=\"0\"/>\n");
+    //   expect(fooBar.setAttributes({a:-1}).render()).toEqual("<fooBar a=\"-1\"/>\n");
+    //   expect(fooBar.setAttributes({a:7}).render()).toEqual("<fooBar a=\"7\"/>\n");
+    // });
+    // 
+    // it("renders floats", function(){
+    //   expect(fooBar.setAttributes({a:0.0}).render()).toEqual("<fooBar a=\"0\"/>\n");
+    //   
+    //   expect(fooBar.setAttributes({a:0.01}).render()).toEqual("<fooBar a=\"0.01\"/>\n");
+    //   expect(fooBar.setAttributes({a:0.0100}).render()).toEqual("<fooBar a=\"0.01\"/>\n");
+    //   
+    //   expect(fooBar.setAttributes({a:23.45}).render()).toEqual("<fooBar a=\"23.45\"/>\n");
+    //   expect(fooBar.setAttributes({a:-23.45}).render()).toEqual("<fooBar a=\"-23.45\"/>\n");
+    // });
+    // 
+    // it("does the javascript thing on really big and really small numbers.  just calling this out.", function(){
+    //   expect(fooBar.setAttributes({a:70000000000000000000000}).render()).toEqual("<fooBar a=\"7e+22\"/>\n");
+    //   expect(fooBar.setAttributes({a:70000000000000000000000.01}).render()).toEqual("<fooBar a=\"7e+22\"/>\n");
+    //   
+    //   expect(fooBar.setAttributes({a:0.00000000000000000000007}).render()).toEqual("<fooBar a=\"7e-23\"/>\n");
+    // });
+    
+    //todo: throw if the attribute value is a non-primitive?
+  });
   // 
   // describe("element children", function() {
   //   it("renders children as inner tags", function(){
