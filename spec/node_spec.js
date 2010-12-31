c9882f04e535c65a0f3841138052968b963be7b6
Steve Conover <sconover@gmail.com>
2010-12-31 10:50:56 -0800

    node basics

---
+++ spec/node_spec.js
@@ -0,0 +1,187 @@
+require("./spec_helper.js");
+
+describe("Jbob: nodes", function() {
+  
+  beforeEach(function(){
+    _ = {}
+    Jbob.apply(_, [{tags:["fooBar", "x", "y", "iDontSelfClose"], nonSelfClosingTags:["iDontSelfClose"]}])
+  });
+  
+  describe("tag closing", function() {
+    it("self-closes by default", function(){
+      expect(_.fooBar()).
+     toEqual("<fooBar/>\n");
+    });
+
+    it("doesn't self-close for non self closing tags", function(){
+      expect(_.iDontSelfClose()).
+     toEqual("<iDontSelfClose></iDontSelfClose>\n");
+    });
+    
+    it("doesn't self-close if there are children.", function(){
+      expect(_.fooBar(_.x())).
+     toEqual("<fooBar>\n" +
+             "<x/>\n" +
+             "</fooBar>\n");
+    });
+    
+    it("doesn't self-close if there are text nodes", function(){
+      expect(_.fooBar("x")).
+     toEqual("<fooBar>x</fooBar>\n");
+    });
+    
+  });
+  
+  // 
+  // describe("attributes", function() {
+  //   it("renders attributes as key value pairs separated by = in the tag", function(){
+  //     expect(fooBar.setAttributes({a:"b"}).render()).
+  //    toEqual("<fooBar a=\"b\"/>\n");
+  //   });
+  // 
+  //   it("renders several attributes in a row.  order is imposed", function(){
+  //     expect(fooBar.setAttributes({x:"y", a:"b", c:"d"}).render()).
+  //    toEqual("<fooBar a=\"b\" c=\"d\" x=\"y\"/>\n");
+  //   });
+  // 
+  //   it("renders booleans", function(){
+  //     expect(fooBar.setAttributes({a:true}).render()).toEqual("<fooBar a=\"true\"/>\n");
+  //     expect(fooBar.setAttributes({a:false}).render()).toEqual("<fooBar a=\"false\"/>\n");
+  //   });
+  // 
+  //   it("renders integers", function(){
+  //     expect(fooBar.setAttributes({a:0}).render()).toEqual("<fooBar a=\"0\"/>\n");
+  //     expect(fooBar.setAttributes({a:-1}).render()).toEqual("<fooBar a=\"-1\"/>\n");
+  //     expect(fooBar.setAttributes({a:7}).render()).toEqual("<fooBar a=\"7\"/>\n");
+  //   });
+  //   
+  //   it("renders floats", function(){
+  //     expect(fooBar.setAttributes({a:0.0}).render()).toEqual("<fooBar a=\"0\"/>\n");
+  // 
+  //     expect(fooBar.setAttributes({a:0.01}).render()).toEqual("<fooBar a=\"0.01\"/>\n");
+  //     expect(fooBar.setAttributes({a:0.0100}).render()).toEqual("<fooBar a=\"0.01\"/>\n");
+  // 
+  //     expect(fooBar.setAttributes({a:23.45}).render()).toEqual("<fooBar a=\"23.45\"/>\n");
+  //     expect(fooBar.setAttributes({a:-23.45}).render()).toEqual("<fooBar a=\"-23.45\"/>\n");
+  //   });
+  //   
+  //   it("does the javascript thing on really big and really small numbers.  just calling this out.", function(){
+  //     expect(fooBar.setAttributes({a:70000000000000000000000}).render()).toEqual("<fooBar a=\"7e+22\"/>\n");
+  //     expect(fooBar.setAttributes({a:70000000000000000000000.01}).render()).toEqual("<fooBar a=\"7e+22\"/>\n");
+  //     
+  //     expect(fooBar.setAttributes({a:0.00000000000000000000007}).render()).toEqual("<fooBar a=\"7e-23\"/>\n");
+  //   });
+  //   
+  //   //todo: throw if the attribute value is a non-primitive?
+  // });
+  // 
+  // describe("element children", function() {
+  //   it("renders children as inner tags", function(){
+  //     expect(fooBar.addChild(new Jaml.Node("x")).render()).
+  //    toEqual("<fooBar>\n" +
+  //            "  <x/>\n" +
+  //            "</fooBar>\n");
+  //   });
+  // 
+  //   it("renders multiple children in order", function(){
+  //     expect(fooBar.addChild(new Jaml.Node("x")).
+  //                   addChild(new Jaml.Node("y")).
+  //                   addChild(new Jaml.Node("z")).render()).
+  //    toEqual("<fooBar>\n" +
+  //            "  <x/>\n" +
+  //            "  <y/>\n" +
+  //            "  <z/>\n" +
+  //            "</fooBar>\n");
+  //   });
+  // 
+  //   it("renders children of children", function(){
+  //     expect(fooBar.addChild(new Jaml.Node("x").
+  //                     addChild(new Jaml.Node("J"))).
+  //                   addChild(new Jaml.Node("y").
+  //                     addChild(new Jaml.Node("K"))).render()).
+  //    toEqual("<fooBar>\n" +
+  //            "  <x>\n" +
+  //            "    <J/>\n" +
+  //            "  </x>\n" +
+  //            "  <y>\n" +
+  //            "    <K/>\n" +
+  //            "  </y>\n" +
+  //            "</fooBar>\n");
+  //   });
+  // });
+  // 
+  // describe("array children", function() {
+  //   it("renders an array of nodes as a series of child nodes.  " +
+  //      "this is often useful when you want to $.map or _.map an array of objects into the equivalent array of nodes.", function(){
+  //     expect(fooBar.addChild([new Jaml.Node("x"), new Jaml.Node("y")]).
+  //                   addChild(new Jaml.Node("z")).render()).
+  //    toEqual("<fooBar>\n" +
+  //            "  <x/>\n" +
+  //            "  <y/>\n" +
+  //            "  <z/>\n" +
+  //            "</fooBar>\n");
+  //   });    
+  // 
+  //   it("works with mixed node and textnodes", function(){
+  //     expect(fooBar.addChild([new Jaml.Node("x"), new Jaml.TextNode("y")]).
+  //                   addChild(new Jaml.TextNode("z")).render()).
+  //    toEqual("<fooBar>\n" +
+  //            "  <x/>\n" +
+  //            "yz</fooBar>\n");
+  //   });    
+  // 
+  //   it("renders arrays of arrays too", function(){
+  //      expect(fooBar.addChild([[new Jaml.Node("x"), [new Jaml.Node("y")]]]).
+  //                    addChild(new Jaml.Node("z")).render()).
+  //     toEqual("<fooBar>\n" +
+  //             "  <x/>\n" +
+  //             "  <y/>\n" +
+  //             "  <z/>\n" +
+  //             "</fooBar>\n");
+  //   });    
+  // 
+  //   it("renders children of children in arrays properly", function(){
+  //     expect(fooBar.addChild([[new Jaml.Node("x").addChild(new Jaml.Node("J")), 
+  //                             [new Jaml.Node("y").addChild(new Jaml.Node("K"))]]]).
+  //                   addChild(new Jaml.Node("z")).render()).
+  //    toEqual("<fooBar>\n" +
+  //            "  <x>\n" +
+  //            "    <J/>\n" +
+  //            "  </x>\n" +
+  //            "  <y>\n" +
+  //            "    <K/>\n" +
+  //            "  </y>\n" +
+  //            "  <z/>\n" +
+  //            "</fooBar>\n");
+  //   });    
+  // 
+  //   it("renders array children, plus attributes, properly (bug)", function(){
+  //     expect(fooBar.setAttributes({x:"y"}).
+  //                   addChild([new Jaml.Node("x"), new Jaml.Node("y")]).render()).
+  //    toEqual("<fooBar x=\"y\">\n" +
+  //            "  <x/>\n" +
+  //            "  <y/>\n" +
+  //            "</fooBar>\n");
+  //   });    
+  // 
+  // })
+  // 
+  // describe("textnode children", function() {
+  //   it("renders", function(){
+  //     expect(new Jaml.TextNode("x").render()).
+  //    toEqual("x");
+  //   });
+  // 
+  //   it("renders a single textnode child all on one line", function(){
+  //     expect(new Jaml.Node("fooBar").addChild(new Jaml.TextNode("x")).render()).
+  //    toEqual("<fooBar>x</fooBar>\n");
+  //   });
+  // 
+  //   it("renders text nodes in one line, on a new line if there are multiple textnode children", function(){
+  //     expect(fooBar.addChild(new Jaml.TextNode("x")).
+  //                   addChild(new Jaml.TextNode("y")).render()).
+  //    toEqual("<fooBar>\nxy</fooBar>\n");
+  //   });
+  // });
+});
+
---
+++ spec/template_spec_x.js
@@ -0,0 +1,142 @@
+require("./spec_helper.js");
+
+describe("Jaml.Template", function() {
+  
+  describe("all html tags", function() {
+    it("a giant integration test for all html tags, so we see what we're allowing." +
+       "  intentionally locating this at the top of this spec file.", function(){      
+      expect(new Jaml.Template(function(){
+               html(
+                 head(
+                   meta(), script(), title(), link()
+                 ),
+                 body(
+                   div(), p(), span(), a(), img(),
+                   br(), hr(),
+                   table(
+                     thead(tr(th())),
+                     tbody(tr(td()))
+                   ),
+                   ul(li(), ol()),
+                   dl(), dt(), dd(),
+                   h1(), h2(), h3(), h4(), h5(), h6(), h7(),
+                   form(
+                     fieldset(
+                       label(),
+                       input(),
+                       textarea(),
+                       button()
+                     )
+                   )
+                 )
+               )
+             }).render()).
+     toEqual("<html>\n" +
+             "  <head>\n" +
+             "    <meta/>\n    <script></script>\n    <title/>\n    <link/>\n" +
+             "  </head>\n" +
+             "  <body>\n" +
+             "    <div/>\n    <p/>\n    <span/>\n    <a/>\n    <img/>\n" +
+             "    <br/>\n    <hr/>\n" +
+             "    <table>\n" +
+             "      <thead>\n        <tr>\n          <th/>\n        </tr>\n      </thead>\n" +
+             "      <tbody>\n        <tr>\n          <td/>\n        </tr>\n      </tbody>\n" +
+             "    </table>\n" +
+             "    <ul>\n      <li/>\n      <ol/>\n    </ul>\n" +
+             "    <dl/>\n    <dt/>\n    <dd/>\n" +
+             "    <h1/>\n    <h2/>\n    <h3/>\n    <h4/>\n    <h5/>\n    <h6/>\n    <h7/>\n" +
+             "    <form>\n" +
+             "      <fieldset>\n" +
+             "        <label/>\n" +
+             "        <input/>\n" +
+             "        <textarea></textarea>\n" + //I'm not self-closing
+             "        <button/>\n" +
+             "      </fieldset>\n" +
+             "    </form>\n" +
+             "  </body>\n" +
+             "</html>\n");
+    });
+  });
+  
+  describe("basic", function() {
+    it("renders", function(){
+      expect(new Jaml.Template(function(){
+               ul(
+                 li("red"),
+                 li("green")
+               )
+             }).render()).
+     toEqual("<ul>\n" +
+             "  <li>red</li>\n" +
+             "  <li>green</li>\n" + 
+             "</ul>\n");
+    });
+    
+    it("renders with data", function(){
+      var theWidget = {primaryColor: "red", secondaryColor: "green"}
+      
+      expect(new Jaml.Template(function(widget){
+               ul(
+                 li(widget.primaryColor),
+                 li(widget.secondaryColor)
+               )
+             }).render(theWidget)).
+     toEqual("<ul>\n" +
+             "  <li>red</li>\n" +
+             "  <li>green</li>\n" + 
+             "</ul>\n");
+    });    
+  });
+  
+  describe("array data", function() {
+    it("renders the template for each item in the array", function(){
+      expect(new Jaml.Template(function(widget){
+               ul(
+                 li(widget.primaryColor),
+                 li(widget.secondaryColor)
+               )
+             }).render([
+               {primaryColor: "red", secondaryColor: "green"},
+               {primaryColor: "orange", secondaryColor: "blue"},
+               {primaryColor: "yellow", secondaryColor: "purple"}
+             ])).
+     toEqual("<ul>\n" +
+             "  <li>red</li>\n" +
+             "  <li>green</li>\n" + 
+             "</ul>\n" +
+             "<ul>\n" +
+             "  <li>orange</li>\n" +
+             "  <li>blue</li>\n" + 
+             "</ul>\n" +
+             "<ul>\n" +
+             "  <li>yellow</li>\n" +
+             "  <li>purple</li>\n" + 
+             "</ul>\n");
+    });    
+  });
+  
+  describe("calling context is available to jaml templates", function() {
+  
+    it("is able to use values of variables defined outside of the template", function(){
+      var myColor = "purple"
+      var yourColor = "orange"
+      
+      expect(new Jaml.Template(function(widget){
+               ul(
+                 li(purple),
+                 li(orange)
+               )
+             }).render([
+               {primaryColor: "red", secondaryColor: "green"}
+             ])).
+     toEqual("<ul>\n" +
+             "  <li>purple</li>\n" +
+             "  <li>orange</li>\n" + 
+             "</ul>\n");
+    });    
+    
+  });
+  
+  
+});
+
